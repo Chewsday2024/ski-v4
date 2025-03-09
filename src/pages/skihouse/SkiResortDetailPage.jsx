282bd7c914189381c4801d9eb5b2748addce905d
WJIA37 <r810307@yahoo.com.tw>
2025-03-09 17:40:31 +0800

    update resort

--- src/pages/skihouse/SkiResortDetailPage.jsx
+++ src/pages/skihouse/SkiResortDetailPage.jsx
@@ -3,6 +3,10 @@ import './SkiHouse.scss';
 import axios from 'axios';
 import { NavLink, useParams, useNavigate  } from 'react-router';
 import LevelProgressBar from './resortComps/LevelProgressBar';
+import BackToTopButton from '../../components/BackToTopButton';
+import AOS from "aos";
+import "aos/dist/aos.css"; // 引入 AOS 的 CSS 样式
+import BlurText from './resortComps/BlurText';
 
 
 
@@ -31,11 +35,13 @@ export default  function SkiResortDetailPage() {
         setCoaches(res.data);
         
       } catch (error) {
-        alert(`Error: ${error.message}`);
+        console.log(`Error: ${error.message}`);
       }
     };
     getCoaches(id);
   },[])
+
+  AOS.init();
 
   //格式化tag名稱
   const formatTagName = (key) => {
@@ -57,6 +63,7 @@ export default  function SkiResortDetailPage() {
   const filteredCoaches = coaches.filter(coach => skiResorts.selectCoach?.includes(coach.id));
 
   const handleBookingCoach =(id) =>{
+    console.log("Navigating to coach ID:", id);
     navigate(`/coach/${id}`);
   }
   
@@ -71,6 +78,7 @@ export default  function SkiResortDetailPage() {
   return (
     <>
       <div className="container mt-4">
+        <BackToTopButton />
         <div className="breadcrumb">
           <nav aria-label="breadcrumb">
               <ol className="breadcrumb">
@@ -81,19 +89,27 @@ export default  function SkiResortDetailPage() {
               </ol>
           </nav>
         </div>
-        <img className="resortImg img-fluid vw-100  rounded object-fit-cover"
+        <img className="resortImg img-fluid vw-100  rounded object-fit-cover" data-aos="flip-left"
         src={skiResorts.image} alt={skiResorts.chineseName} />
 
         <div className="resortDetail ">
-          <h1 className="resortName text-center my-4 fs-1 fw-bolder">{skiResorts.chineseName}</h1>
-          <div className="resortTag d-flex flex-wrap justify-content-center gap-2 mb-3">
+          <BlurText
+            text={skiResorts.chineseName}
+            delay={150}
+            animateBy="words"
+            direction="top"
+            className="resortName text-center my-4 fs-1 fw-bolder pt-3 text-brand-01"
+          />
+          <div className="resortTag d-flex flex-wrap justify-content-center gap-2 mb-3 "
+          data-aos="fade-right" data-aos-duration="2500" data-aos-delay="300">
             {Object.entries(skiResorts.tag || {}).filter(([_, value]) => value).map(([key]) =>(<button key={key} type="button" className="tagBtn btn btn-outline-brand-02 fs-6 fw-bolder">{formatTagName(key)}</button>
             ))}
             
         </div>
-          <p className="my-3 fs-5">{skiResorts.description}</p>
+          <p className="my-3 fs-5 m-3" data-aos="fade-left" data-aos-duration="2500" data-aos-delay="200">{skiResorts.description}</p>
           <div className="row row-cols-2 row-cols-lg-6 gy-1 my-3">
-            <div className="col">
+            <div className="col" data-aos="flip-down" data-aos-duration="2500"
+            data-aos-delay="100">
             <div className="card border-brand-02 mb-3 text-center" >
               <h5 className="cardHeader card-header bg-brand-02 text-light">滑雪道數量</h5>
               <div className="card-body text-dark">
@@ -101,7 +117,8 @@ export default  function SkiResortDetailPage() {
               </div>
             </div>
             </div>
-            <div className="col">
+            <div className="col" data-aos="flip-down" data-aos-duration="2500"
+            data-aos-delay="200">
             <div className="card border-brand-02 mb-3 text-center" >
               <h5 className="cardHeader card-header bg-brand-02 text-light">纜車數量</h5>
               <div className="card-body text-dark">
@@ -109,7 +126,8 @@ export default  function SkiResortDetailPage() {
               </div>
             </div>
             </div>
-            <div className="col">
+            <div className="col" data-aos="flip-down" data-aos-duration="2500"
+            data-aos-delay="300">
             <div className="card border-brand-02 mb-3 text-center" >
               <h5 className="cardHeader card-header bg-brand-02 text-light">最長坡道</h5>
               <div className="card-body text-dark">
@@ -117,7 +135,8 @@ export default  function SkiResortDetailPage() {
               </div>
             </div>
             </div> 
-            <div className="col">
+            <div className="col" data-aos="flip-down" data-aos-duration="2500"
+            data-aos-delay="400">
             <div className="card border-brand-02 mb-3 text-center" >
               <h5 className="cardHeader card-header bg-brand-02 text-light">最大斜度</h5>
               <div className="card-body text-dark">
@@ -125,7 +144,8 @@ export default  function SkiResortDetailPage() {
               </div>
             </div>
             </div>
-            <div className="col">
+            <div className="col" data-aos="flip-down" data-aos-duration="2500"
+            data-aos-delay="500">
             <div className="card border-brand-02 mb-3 text-center" >
               <h5 className="cardHeader card-header bg-brand-02 text-light">最低海拔</h5>
               <div className="card-body text-dark">
@@ -133,7 +153,8 @@ export default  function SkiResortDetailPage() {
               </div>
             </div>
             </div>
-            <div className="col">
+            <div className="col" data-aos="flip-down" data-aos-duration="2500"
+            data-aos-delay="600">
             <div className="card border-brand-02 mb-3 text-center" >
               <h5 className="cardHeader card-header bg-brand-02 text-light">最高海拔</h5>
               <div className="card-body text-dark">
@@ -145,22 +166,20 @@ export default  function SkiResortDetailPage() {
           
           <LevelProgressBar />
 
-
-          <h3 className="orderCoach my-3 pt-5 text-center fs-2 fw-bold">快來預約我們滑雪場的教練吧！</h3>
+          <h3 className="orderCoach my-3 pt-5 text-center text-brand-01 fs-2 fw-bold" data-aos="zoom-out-up" data-aos-duration="2500" data-aos-delay="200">快來預約我們滑雪場的教練吧！</h3>
           <div className="row row-cols-1 row-cols-lg-3 justify-content-center pt-3">
               {filteredCoaches.map(coach =>(
-                <div key={coach.id} className="col-md-4 d-flex justify-content-around flex-column align-items-center my-4">
-                <img src={coach.profileImg} className="img-fluid rounded-circle object-fit-cover  mb-3" alt={coach.name} style={{height:"200px", width:"200px"}} />
+                <div key={coach.id} className="col-md-4 d-flex justify-content-around flex-column align-items-center my-4" data-aos="zoom-out-up" data-aos-duration="2500" data-aos-delay="200">
+                <img src={coach.profileImg} className="coachProfileImg img-fluid rounded-circle object-fit-cover  mb-3" alt={coach.name} style={{height:"200px", width:"200px"}} />
                 <button onClick={() =>handleBookingCoach(coach.id)}
                 type="button" className="orderCoachBtn btn btn-brand-01 rounded-5 fs-5" style={{width: "150px"}}>預約教練</button>
               </div>
             ))}
             <button onClick={handleSeeMore}
             type="button" className="seeMore btn btn-outline-brand-01 rounded-5 m-5
-            text-center fs-4 fw-bold" style={{width: "200px"}}>查看更多<span className="material-symbols-outlined align-bottom ms-3 mb-1">arrow_circle_right</span></button>
+            text-center fs-4 fw-bold" data-aos="zoom-out-down" data-aos-duration="2500" data-aos-delay="200" style={{width: "200px"}}>查看更多<span className="material-symbols-outlined align-bottom ms-3 mb-1">arrow_circle_right</span></button>
           </div>
         </div>
-
       </div>
     </>
   )
